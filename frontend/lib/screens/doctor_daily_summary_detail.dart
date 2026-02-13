ad9f589d64a20fc03c91f929c23e557861ad45b9
Kriththigan <kriththiganklp10@gmail.com>
2026-02-13 11:23:09 +0530

    Created daily summary detail screen

---
+++ frontend/lib/screens/doctor_daily_summary_detail.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import '../config/app_theme.dart';
+
+class DoctorDailySummaryDetail extends StatelessWidget {
+  final int day; 
+
+  const DoctorDailySummaryDetail({super.key, required this.day});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8FAFC), 
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // --- Header ---
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Daily Summary for\n${day}th November,\n2025', 
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        height: 1.2, 
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.close, size: 28),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+              
+              const SizedBox(height: 30),
+
+              // --- Scrollable Text Area ---
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF2FBF9), // Light Mint background
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.grey.withOpacity(0.3)),
+                  ),
+                  child: const SingleChildScrollView(
+                    child: Text(
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\n"
+                      "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n"
+                      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        height: 1.5, 
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20), // Spacing before bottom nav
+            ],
+          ),
+        ),
+      ),
+
+      // --- Bottom Navigation Bar ---
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, -4),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+             _buildNavItem(Icons.home_filled, "Home", false),
+             _buildNavItem(Icons.menu_book_outlined, "Trends &\nsummary", true),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, String label, bool isSelected) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: isSelected ? Colors.black : Colors.grey, size: 28),
+        const SizedBox(height: 4),
+        Text(
+          label, 
+          textAlign: TextAlign.center, 
+          style: TextStyle(
+            fontSize: 10, 
+            color: isSelected ? Colors.black : Colors.grey,
+            fontWeight: isSelected ? FontWeight.w600 : FontWeight.normal,
+          )
+        ),
+      ],
+    );
+  }
+}
